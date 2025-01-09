8daa205877b146cb6f51726ed023db64d00a5670
aidan gollan <aidangollan@icloud.com>
2025-01-09 14:06:57 -0800

    nerd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "talk tuah nerd",
+  title: "flappy nerd",
   description: "hack tuah, code on that thang.",
   icons: [{ rel: "icon", url: "/galaxy.ico" }],
 };
